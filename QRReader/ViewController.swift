9a0e0cdd4ef0be0ece4e8345c06c0c8462779861
pateldevang <devangpatel92@gmail.com>
2019-08-25 13:39:18 +0530

    supported code type added so scanner can scan barcode, QRcode etc.

--- QRReader/ViewController.swift
+++ QRReader/ViewController.swift
@@ -12,6 +12,22 @@ import AVFoundation
 class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     var video = AVCaptureVideoPreviewLayer()
+    
+    //Supported code types also included bar code reader
+    private let supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
+                                      AVMetadataObject.ObjectType.code39,
+                                      AVMetadataObject.ObjectType.code39Mod43,
+                                      AVMetadataObject.ObjectType.code93,
+                                      AVMetadataObject.ObjectType.code128,
+                                      AVMetadataObject.ObjectType.ean8,
+                                      AVMetadataObject.ObjectType.ean13,
+                                      AVMetadataObject.ObjectType.aztec,
+                                      AVMetadataObject.ObjectType.pdf417,
+                                      AVMetadataObject.ObjectType.itf14,
+                                      AVMetadataObject.ObjectType.dataMatrix,
+                                      AVMetadataObject.ObjectType.interleaved2of5,
+                                      AVMetadataObject.ObjectType.qr]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
