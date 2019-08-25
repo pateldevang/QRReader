ea606cbd92cbf21e6a57c61cd14ce2afcd9fca96
pateldevang <devangpatel92@gmail.com>
2019-08-25 12:08:37 +0530

    Initial Commit

---
+++ QRReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRReader
+//
+//  Created by Devang Patel on 25/08/19.
+//  Copyright © 2019 Devang Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
